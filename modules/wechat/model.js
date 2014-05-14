17a18e2ac04e370ee26bc4aa10fb46660306b03f
albin3 <binwei.zeng3@gmail.com>
2014-05-14 14:42:02 +0800

    new_menu

--- modules/wechat/model.js
+++ modules/wechat/model.js
@@ -8,7 +8,11 @@ var db = mongojs(config.dbinfo.dbname);
 var dbuvindex = db.collection('account');
 var ObjectID = require('mongodb').ObjectID;
 
-exports.restomsg = function (params,callback) {
-  return callback({ret: 1});
+exports.autoreply_txt = function (message,req,res) {
+console.log(message);
+var input = (message.Content || '').trim();
+//用户发送信息时的回复
+var from = message.FromUserName;
+return res.reply("功能研发中，敬请期待！");
 };
 
--- modules/wechat/view.js
+++ modules/wechat/view.js
@@ -3,13 +3,7 @@ var model = require('./model'),
     wechat = require('wechat');
 // 微信路由
 exports.wechat = wechat('qweruiop',wechat.text(function (message, req, res) {
-  console.log(message);
-  var input = (message.Content || '').trim();
-        //用户发送信息时的回复
-  //restomsg(input,function(err, retailers){
-  var from = message.FromUserName;
-  return res.reply("功能研发中，敬请期待！");
-  //}
+model.autoreply_txt(message, req,  res);
 }).location(function (message, req, res)
 {
 
@@ -22,7 +16,7 @@ exports.wechat = wechat('qweruiop',wechat.text(function (message, req, res) {
         // 用户使用菜单时的消息
     switch(message.EventKey){
 
-      case 'V1001_SuoPei':
+      case 'V1005_SuoPei':
       return  res.reply('请回复您所需查询事项的序号： \n' +
                         ' 1.单方事故 \n' +
                         ' 2.双方事故 \n' +
@@ -32,7 +26,7 @@ exports.wechat = wechat('qweruiop',wechat.text(function (message, req, res) {
                         ' 6.盗抢事故 ');
       break;
 
-      case 'V1002_VIP':
+      case 'V1004_VIP':
       return  res.reply('您尚未绑定手机号！\n ' + 
                         ' <a href="http://183.61.111.195/static/verification_VIP.html">请点击此处绑定手机号！</a> '
                        );
@@ -42,11 +36,11 @@ exports.wechat = wechat('qweruiop',wechat.text(function (message, req, res) {
       return  res.reply('请回复查询项目的序号：\n 1.出险次数 \n 2.赔付金额 \n 3.理赔状态');
       break;
 
-      case 'V1004_AnJian':
+      case 'V1002_AnJian':
       return  res.reply('请回复查询条件的序号： \n 1.车牌号 \n  2.保单号 \n 3.报案号');
       break;
 
-      case 'V1005_BaoAn':
+      case 'V1001_BaoAn':
       return  res.reply('请点选报案类型：' +
                          '\n' +
                          '\t\t\t\t\t\t\t\t\t' +
@@ -62,7 +56,7 @@ exports.wechat = wechat('qweruiop',wechat.text(function (message, req, res) {
                          '<a href="http://183.61.111.195/static/Report.html">非车险报案</a>');
       break;
 
-      case 'V2001_ChuShen':
+      case 'V2003_ChuShen':
       return  res.reply('您尚未绑定手机号！\n' +
                         ' <a href="http://183.61.111.195/static/verification_ORG.html">请点击此处绑定手机号！</a> ' 
                        );
@@ -74,13 +68,13 @@ exports.wechat = wechat('qweruiop',wechat.text(function (message, req, res) {
               );
       break;
 
-      case 'V2003_GaiPai':
+      case 'V2001_GaiPai':
       return  res.reply('您尚未绑定手机号！\n' +
                         ' <a href="http://183.61.111.195/static/verification_ORG.html">请点击此处绑定手机号！</a> '
                        );
       break;
 
-      case 'V3001_ShouDan':
+      case 'V3003_ShouDan':
       return  res.reply('即将开放');
       break;
 
@@ -88,7 +82,7 @@ exports.wechat = wechat('qweruiop',wechat.text(function (message, req, res) {
       return  res.reply('请输入任务派遣工短信的全部内容，谢谢！');
       break;
 
-      case 'V3003_DaiJia':
+      case 'V3001_DaiJia':
       return  res.reply('即将开放');
       break;
 
